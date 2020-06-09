9e38189c40a247859cacbfcf9bb0c1b608dc078c
drussell1974 <dave@jazzthecat.co.uk>
2020-06-09 10:05:53 +0100

    #4 create docker-entrypoint script to look at path for ldif files that can be run when container starts

--- openldap-ubuntu/custom/init/README.md
+++
@@ -1,2 +0,0 @@
-place ldif files that should be executed when the container is run 
-
--- openldap-ubuntu/docker-entrypoint.sh
+++ openldap-ubuntu/docker-entrypoint.sh
@@ -1,8 +1,38 @@
-#!/bin/bash
+#!/bin/sh
+
+_ldapadd_init_ldif() {
+
+	# 1. check init directory and file variables have set
+	
+	# 2. check init directory and init file exists
+
+	# 3. check the base dn, admin user and admin password has been set
+	
+	# 4. command should be format e.g. ldapadd -w <PASSWORD> -D "cn=admin,dc=daverussell,dc=co,dc=uk" -f custom/init/1_add_group.ldif
+
+	if test -f ${LDAP_INIT_DIR:="init"}/${LDAP_INIT_FILE:="init.ldif"}; then
+		if [ ${LDAP_BASE_DN:-$LDAP_BASE_DN} -a ${LDAP_ADMIN_USER:-$LDAP_ADMIN_USER} -a ${LDAP_ADMIN_PASSWORD:-$LDAP_ADMIN_PASSWORD} ]; then 
+			
+			echo "docker-entrypoint.sh: adding ldif to ldap directory ${LDAP_ADMIN_USER},${LDAP_BASE_DN}"
+
+			# add the record...
+
+			ldapadd -w "${LDAP_ADMIN_PASSWORD}" -D "${LDAP_ADMIN_USER},${LDAP_BASE_DN}" -f $LDAP_INIT_DIR/$LDAP_INIT_FILE
+		
+		else
+			echo "docker-entrypoint.sh: Cannot 'ldapadd ... ${LDAP_INIT_DIR}/${LDAP_INIT_FILE}', as LDAP_BASE_DN, LDAP_ADMIN_USER or LDAP_ADMIN_PASSWORD not set."
+		fi
+	fi
+}
 
 _main() {
+
 	service slapd start
-	echo "init..."
+
+	_ldapadd_init_ldif 
+	
+	# keep alive
+
 	bash
 }
 
